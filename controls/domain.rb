bbdaa640ffbb8c42a0296252c49f831762901f3a
juju4 <juju4@users.noreply.github.com>
2020-02-01 14:29:54 -0500

    add optional dcdiag check

--- controls/domain.rb
+++ controls/domain.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 domain_enabled = input('domain_enabled', value: true, description: 'Check Domain Controller role is configured')
+domain2_enabled = input('domain2_enabled', value: false, description: 'Check Domain Controller role is active - post-reboot')
 
 if domain_enabled
   title 'Server is domain controller'
@@ -16,6 +17,21 @@ if domain_enabled
     desc 'Ensure domain Windows Features are present'
     describe windows_feature('AD-Domain-Services') do
       it { should be_installed }
+    end
+  end
+end
+
+if domain2_enabled
+  title 'Server is active domain controller'
+  control 'domaindiag-01' do
+    title 'Dcdiag'
+    desc 'Ensure dcdiag returns no warnings or errors'
+    describe command('dcdiag /e /v /c') do
+      its('stdout') { should eq '/Directory Server Diagnosis:/' }
+      its('stdout') { should eq '/Testing Server:/' }
+      its('stdout') { should eq '/passed/' }
+      its('stdout') { should_not eq '/Error/' }
+      its('stdout') { should_not eq '/failed/' }
     end
   end
 end
