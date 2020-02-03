98f42f62dcda5b5783c878d3a40a535993c57797
juju4 <juju4@users.noreply.github.com>
2020-02-02 19:40:50 -0500

    add GPOs check

---
+++ controls/gpos.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+gpos_check_enabled = input('gpos_check_enabled', value: true, description: 'Check if expected GPOs are configured')
+gpos_list = input(
+  'gpos_list',
+  value: [
+    'Custom Event Channel Permissions',
+    'Default Domain Policy',
+    'Default Domain Controllers Policy',
+    'Disable Windows Defender',
+    'Domain Controllers Enhanced Auditing Policy',
+    'Powershell Logging',
+    'Servers Enhanced Auditing Policy',
+    'Windows Event Forwarding Server',
+    'Workstations Enhanced Auditing Policy'
+  ],
+  description: 'list of expected GPOs'
+)
+
+if gpos_check_enabled
+  title 'Active GPOs'
+  control 'gpos-01' do
+    title 'Get-GPO'
+    desc 'Ensure expected GPOs are present'
+    describe command('Get-GPO -All') do
+      gpos_list.each do |str|
+        its('stdout') { should eq "/#{str}/" }
+      end
+    end
+  end
+end
